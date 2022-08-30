fa9eb80909efe7fd19b4ef6a63a987af5411ae21
BrandonVanek <bvanek@skillstorm.com>
2022-08-30 14:22:21 -0400

    Finished userplan controller, beginning testing

--- PhoneService/Controllers/UserPlansController.cs
+++ PhoneService/Controllers/UserPlansController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PhoneService.Data;
+using PhoneService.DTO;
 using PhoneService.Models;
 
 namespace PhoneService.Controllers
@@ -32,65 +33,82 @@ namespace PhoneService.Controllers
             return await _context.UserPlans.ToListAsync();
         }
 
-        // GET: api/UserPlans/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<UserPlan>> GetUserPlan(int id)
-        {
-          if (_context.UserPlans == null)
-          {
-              return NotFound();
-          }
-            var userPlan = await _context.UserPlans.FindAsync(id);
+        //// GET: api/UserPlans/5
+        //[HttpGet("{id}")]
+        //public async Task<ActionResult<UserPlan>> GetUserPlan(int id)
+        //{
+        //  if (_context.UserPlans == null)
+        //  {
+        //      return NotFound();
+        //  }
+        //    var userPlan = await _context.UserPlans.FindAsync(id);
 
-            if (userPlan == null)
-            {
-                return NotFound();
-            }
+        //    if (userPlan == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return userPlan;
-        }
+        //    return userPlan;
+        //}
 
-        // PUT: api/UserPlans/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutUserPlan(int id, UserPlan userPlan)
-        {
-            if (id != userPlan.Id)
-            {
-                return BadRequest();
-            }
+        //// PUT: api/UserPlans/5
+        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutUserPlan(int id, UserPlan userPlan)
+        //{
+        //    if (id != userPlan.Id)
+        //    {
+        //        return BadRequest();
+        //    }
 
-            _context.Entry(userPlan).State = EntityState.Modified;
+        //    _context.Entry(userPlan).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!UserPlanExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!UserPlanExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-            return NoContent();
-        }
+        //    return NoContent();
+        //}
 
         // POST: api/UserPlans
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<UserPlan>> PostUserPlan(UserPlan userPlan)
+        public async Task<ActionResult<UserPlan>> PostUserPlan(UserPlanDTO userPlanDto)
         {
           if (_context.UserPlans == null)
           {
               return Problem("Entity set 'PhoneServiceDbContext.UserPlans'  is null.");
           }
+            var plan = await _context.Plans.FirstOrDefaultAsync(f => f.Id == userPlanDto.PlanId);
+            var user = await _context.Users.FirstOrDefaultAsync(p => p.Id == userPlanDto.UserId);
+
+            if (plan == null || user == null)
+            {
+                return Problem("User or Plan does not exist.");
+            }
+
+            var userPlan = new UserPlan()
+            {
+                UserId = userPlanDto.UserId,
+                PlanId = userPlanDto.PlanId,
+                User = user,
+                Plan = plan
+            };
             _context.UserPlans.Add(userPlan);
+            plan.UserPlans.Add(userPlan);
+            user.UserPlans.Add(userPlan);
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetUserPlan", new { id = userPlan.Id }, userPlan);
@@ -108,6 +126,15 @@ namespace PhoneService.Controllers
             if (userPlan == null)
             {
                 return NotFound();
+            }
+            if (userPlan.User != null)
+            {
+                userPlan.User.UserPlans.Remove(userPlan);
+            }
+
+            if (userPlan.Plan != null)
+            {
+                userPlan.Plan.UserPlans.Remove(userPlan);
             }
 
             _context.UserPlans.Remove(userPlan);
